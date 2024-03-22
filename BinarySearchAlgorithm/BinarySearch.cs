2add48879ec4df926a52078e2935b8153d2f017c
SPYROS\Spyros <sponaris@gmail.com>
2024-03-22 13:30:34 +0200

    Add project files.

---
+++ BinarySearchAlgorithm/BinarySearch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BinarySearchAlgorithm
+{
+    internal class BinarySearch
+    {
+        public static int Search(int[] arr, int size, int value)
+        {
+            var mid = 0;
+            int low  = 0;
+            int high = size - 1;
+
+            //At each step, we compare the middle value with the value we are
+            //searching.If mid value is equal to the value we are searching for then
+            //we return the middle index.
+
+            while (low <= high)
+            {
+                // If the value is smaller than the middle value, we search the left half of the array.
+
+                mid = low + (high - low) / 2; // to avoid overflow
+
+                if (arr[mid] == value)
+                {
+                    return mid;
+                }
+                else if (arr[mid] < value) 
+                {
+                    //If the value is greater than the middle value then we search the right half of the array.
+
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            // If we find the value we are looking for then its index is returned
+            // otherwise - 1 is returned.
+            return -1;
+        }
+    }
+}
---
+++ BinarySearchAlgorithm/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using BinarySearchAlgorithm;
+
+try
+{
+    int[] arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    Console.WriteLine("Sum of values in array:" + BinarySearch.Search(arr, arr.Length, 7));
+    
+    Console.WriteLine("Sum of values in array:" + BinarySearch.Search(arr, arr.Length, 7));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+    Console.BackgroundColor = ConsoleColor.Red;
+}
+
+Console.ReadLine();
