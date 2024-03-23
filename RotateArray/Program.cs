5a61f186f5b754cc7ba1b0d5419847920c726fd3
SPYROS\Spyros <sponaris@gmail.com>
2024-03-23 22:04:36 +0200

    ReverseArrayClass

---
+++ RotateArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using RotateArray;
+
+try
+{
+    int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+
+    ReverseArrayClass.rotateArray(arr, arr.Length, 2);
+    ReverseArrayClass.PrintArray(arr, arr.Length);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+    Console.BackgroundColor = ConsoleColor.Red;
+}
+
+Console.ReadLine();
---
+++ RotateArray/RreverseArray.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace RotateArray
+{
+    public static class ReverseArrayClass
+    {
+
+        // Rotating list is done in two parts trick.In the first part, we first reverse
+        // elements of list first half and then second half.
+        public static void rotateArray(int[] a, int n, int k)
+        {
+            ReverseArray(a, 0, k - 1);
+            ReverseArray(a, k, n - 1);
+            ReverseArray(a, 0, n - 1);
+        }
+
+        //Then we reverse the whole list there by completing the whole rotation.
+        //• First reversal of the two parts of list is done in O(n) time and the final
+        //list reversal is also done in O(n) so the total time complexity of this
+        //algorithms is O(n)
+
+        public static void ReverseArray(int[] a, int start, int end)
+        {
+            for (int i = start, j = end; i<j;  i++,j--)
+            {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+
+        internal static void PrintArray(int[] arr, int length)
+        {
+            foreach (int i in arr)
+            { 
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
